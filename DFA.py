60816e087e1d59dae38e448dd528bfcf7c697f1a
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-10-26 20:07:14 +0300

    changed read file and added some notes for .txt

--- DFA.py
+++ DFA.py
@@ -2,16 +2,24 @@
     Deterministic finite automation, non Deterministic finite automation
     and NFA with e transitions due to the course of automation theory 7th semester.
 """
-
+import sys
 
 def readFile(fname):
-try:
-    fh = open(fname)
-except Exception as e:
-    print ('Sorry your file cannot be opened ')
-    quit()
+    with open(fname) as f:
+        for line in f:
+            split = line.split(' ')
+            if (split[0].startswith('states')):
+                states = int(split[1])
+            elif (split[0].startswith('initial')):0
+                initial = int(split[1])
+            elif (split[0].startswith('final_states')):
+                final =
+
+
+
+
 
 
 if __name__ == "__main__":
-    fname = input("Enter file name: ") # waiting for the user to give the name.
+    fname = sys.argv[1] # waiting for the user to give the name.
     readFile(fname)
