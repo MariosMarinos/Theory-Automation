7b8fae9259c4ee25f31fcda48ee15854f01f02a5
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-10-28 05:34:25 +0300

    DFA implemented.

--- DFA.py
+++ DFA.py
@@ -4,22 +4,67 @@
 """
 import sys
 
+class DFA:
+    current_state = None;
+    def __init__(self, states, transition_function, start_state, accept_states):
+        self.states = states;
+        self.transition_function = transition_function;
+        self.start_state = start_state;
+        self.accept_states = accept_states;
+        self.current_state = start_state;
+        return;
+
+    def transition_to_state_with_input(self, input_value):
+        if ((self.current_state, input_value) not in self.transition_function.keys()):
+            self.current_state = None;
+            return;
+        self.current_state = self.transition_function[(self.current_state, input_value)];
+        return;
+
+    def in_accept_state(self):
+        return self.current_state in accept_states;
+
+    def go_to_initial_state(self):
+        self.current_state = self.start_state;
+        return;
+
+    def run_with_input_list(self, input_list):
+        self.go_to_initial_state();
+        for inp in input_list:
+            self.transition_to_state_with_input(inp);
+            continue;
+        return self.in_accept_state();
+    pass;
+
 def readFile(fname):
     with open(fname) as f:
+        transitions_dictionary = dict(); # transitions
+        states= list() # all states
+        final_list = list() # final_states in list.
         for line in f:
             split = line.split(' ')
             if (split[0].startswith('states')):
-                states = int(split[1])
-            elif (split[0].startswith('initial')):0
+                for i in range(int(split[1])):
+                    states.append(i+1)
+            elif (split[0].startswith('initial')):
                 initial = int(split[1])
-            elif (split[0].startswith('final_states')):
-                final =
-
-
-
+            elif (split[0].startswith('final')):
+                final = int(split[1])
+            elif (split[0].startswith('f_states')):
+                for i in range(1,final+1):
+                    final_list.append(int(split[i]))
+            elif (split[0].startswith('transitions')):
+                transitions = int(split[1])
+            else:
+                transitions_dictionary[(int(split[0]),split[1])] = int(split[2]);
+    return states, initial, final_list, transitions_dictionary
+    #print ([ (k,v) for k,v in transitions_dictionary.items()])
 
 
 
 if __name__ == "__main__":
     fname = sys.argv[1] # waiting for the user to give the name.
-    readFile(fname)
+    states, initial,accept_states,transitions = readFile(fname)
+    d = DFA(states,transitions,initial,accept_states)
+    inp_program = list('abbbbbbaaaaa');
+    print (d.run_with_input_list(inp_program));
