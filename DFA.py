74cb630a233ae24132d9fce1a708dbceca0de45e
MariosMarinos <38200565+MariosMarinos@users.noreply.github.com>
2019-12-10 04:23:38 +0200

    Final_assign

--- DFA.py
+++ DFA.py
@@ -78,103 +78,15 @@ class NFAe:
             # for each letter go to the next state.
             # if the inp isn't on alphabet exit the program.
             if inp not in self.alphabet:
-                print("The letter(", inp, ")doesn't exist on your alphabet.")
-                exit()
+                print("The letter ({}) doesn't exist on your alphabet.".format(inp))
+                return False
             self.transition_to_state_with_input(inp)
         return self.in_accept_state()
 
 
-class NFA:
-    current_state = None
-
-    def __init__(self, states, transition_function, start_state, accept_states):
-        self.states = states
-        self.transition_function = transition_function
-        self.start_state = start_state
-        self.accept_states = accept_states
-        self.current_state = start_state
-        return
-
-    def transition_to_state_with_input(self, input_value):
-        # if DFA detect a letter that is not supposed to be in our alphabet,
-        # it will set the current state to None. Otherwise if current state and
-        # input value are in the dictionary move to the next state.
-        next_states = set()
-        # iterate each next state in current states.
-        for i in self.current_state:
-            # if input letter doesn't exist set it None and break.
-            if (i, input_value) not in self.transition_function.keys():
-                i = None
-            next_states = next_states | self.transition_function[(i, input_value)]  # intersection
-        if len(next_states) == 0:  # if set is empty it means that the letter
-            # which has been inserted isn't on the alphabet.
-            print("False")
-            exit()
-        else:
-            self.current_state = next_states
-
-    def in_accept_state(self):
-        # if current_state(last letter) is in accpet states then return True otherwise false.
-        for i in self.current_state:
-            if i in accept_states:
-                return True
-        return False
-
-    def go_to_initial_state(self):
-        # set the initial state.
-        self.current_state = self.start_state
-        return
-
-    def run_with_input_list(self, input_list):
-        self.go_to_initial_state()
-        for inp in input_list:  # for each letter go to the next state.
-            self.transition_to_state_with_input(inp)
-        return self.in_accept_state()
-
-
-class DFA:
-    current_state = None
-
-    def __init__(self, states, transition_function, start_state, accept_states):
-        self.states = states
-        self.transition_function = transition_function
-        self.start_state = start_state
-        self.accept_states = accept_states
-        self.current_state = start_state
-        return
-
-    def transition_to_state_with_input(self, input_value):
-        # if DFA detect a letter that is not supposed to be in our alphabet,
-        # it will set the current state to None. Otherwise if current state and
-        # input value are in the dictionary move to the next state.
-        if (self.current_state, input_value) not in self.transition_function.keys():
-            self.current_state = None
-            return
-        self.current_state = self.transition_function[(self.current_state, input_value)]
-        return
-
-    def in_accept_state(self):
-        # if current_state(last letter) is in accpet states then return True otherwise false.
-        return self.current_state in accept_states
-
-    def go_to_initial_state(self):
-        # set the initial state.
-        self.current_state = self.start_state
-        return
-
-    def run_with_input_list(self, input_list):
-        self.go_to_initial_state()
-        for inp in input_list:  # for each letter go to the next state.
-            self.transition_to_state_with_input(inp)
-        return self.in_accept_state()
-
-
-def readFile(fname, falgorithm):
+def readFile(fname):
     with open(fname) as f:
-        if falgorithm == 'DFA':
-            transitions_dictionary = dict()
-        elif falgorithm == 'NFA':
-            transitions_dictionary = defaultdict(set)  # transitions
+        transitions_dictionary = defaultdict(set)  # transitions
         states = set()  # all states
         final_list = set()  # final_states in list.
         for line in f:
@@ -184,10 +96,9 @@ def readFile(fname, falgorithm):
                     states.add(i + 1)
             elif split[0].startswith('initial'):
                 initial = int(split[1])
-                if falgorithm == 'NFA':
-                    temp = set()
-                    temp.add(initial)
-                    initial = temp
+                temp = set()
+                temp.add(initial)
+                initial = temp
             elif split[0].startswith('final'):
                 continue
             elif split[0].startswith('f_states'):
@@ -196,21 +107,21 @@ def readFile(fname, falgorithm):
             elif split[0].startswith('transitions'):
                 transitions = int(split[1])
             else:
-                if falgorithm == 'DFA':
-                    transitions_dictionary[(int(split[0]), split[1])] = int(split[2])
-                else:
-                    transitions_dictionary[(int(split[0]), split[1])].add(int(split[2]))
-
-    print([(k, v) for k, v in transitions_dictionary.items()])
+                transitions_dictionary[(int(split[0]), split[1])].add(int(split[2]))
+    # print([(k, v) for k, v in transitions_dictionary.items()])
 
     return states, initial, final_list, transitions_dictionary, transitions
 
 
 if __name__ == "__main__":
     fname = sys.argv[1]  # waiting for the user to give the name.
-    falgorithm = sys.argv[2]  # tell what kind of problem is. (DFA,NFA,NFA-e)
-    print(fname, falgorithm)
-    states, initial, accept_states, transitions, transitions_num = readFile(fname, falgorithm)
+    states, initial, accept_states, transitions, transitions_num = readFile(fname)
     d = NFAe(states, transitions, initial, accept_states, transitions_num)
-    inp_program = list('0')
-    print(d.run_with_input_list(inp_program))
+    inp_program = list(input("Enter input :"))
+    # while input is not s (stop) give words.
+    while(True):
+        if (inp_program[0] == 's'):
+            print('Exiting program.')
+            exit()
+        print(d.run_with_input_list(inp_program))
+        inp_program = list(input("Enter input :"))
